8bfcb31499b58c88fdc5a136e709ef6bbc136c94
Licornee <yin.dongpeng@foxmail.com>
2022-05-15 10:24:33 +0800

    first commit

---
+++ lexer/token.go
@@ -0,0 +1,5 @@
+package lexer
+
+// 关键字
+// 数据类型 char double enum float int long short signed unsigned volatile -> 10 个
+// 流程控制 break case continue default do else for goto if return switch while -> 12 个
